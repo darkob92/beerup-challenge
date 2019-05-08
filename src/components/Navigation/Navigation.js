efb782dcd23fb8e2d269ffbb06ff958be37bc226
Darko <darko.bogovic92@gmail.com>
2019-05-08 14:44:35 +0200

    added navigation and navigation styles with logo

---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './navigation.scss';
+import Logo from '../../assets/logo.svg';
+import Symbol from '../../assets/symbol.svg';
+
+const Navigation = () => {
+  return(
+    <nav className="navigation">
+      <div className="navigation__logo-box">
+        <img className="logo" src={Symbol} />
+        <img className="logo" src={Logo} />
+      </div>
+
+      <ul className="navigation__list">
+        <li className="navigation__item"><a className="navigation__link" href="#">Home</a></li>
+        <li className="navigation__item"><a className="navigation__link" href="#">Favorites</a></li>
+        <li className="navigation__item"><a className="navigation__link">Join</a></li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navigation;

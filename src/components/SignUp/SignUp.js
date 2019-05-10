7cc38a51fe68c139e99b577840325321cd800e65
Darko <darko.bogovic92@gmail.com>
2019-05-11 00:33:35 +0200

    finishing signup component

--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -18,6 +18,28 @@ class SignUp extends Component {
               <input type="email" placeholder="Email" />
               <input type="number" placeholder="Phone number" />
             </div>
+            <div className="input-container">
+              <label>RSVP</label>
+              <div className="input-container__radio">
+                <input type="radio" name="answer" value="yes" /><span>I'm coming!</span>
+              </div>
+              <div className="input-container__radio">
+                <input type="radio" name="answer" value="maybe" /><span>Maybe?</span>
+              </div>
+              <div className="input-container__radio">
+                <input type="radio" name="answer" value="no" /><span>Can't make it</span>
+              </div>
+              <textarea placeholder="Something you'd like to add?" rows="6" cols="50"></textarea>
+            </div>
+            <div className="input-container">
+              <div className="input-container__radio">
+                <input type="checkbox" name="answer" value="maybe" /><span>Let me know about future beerups!</span>
+              </div>
+              <div className="input-container__radio">
+                <input type="checkbox" name="answer" value="maybe" /><span>Remind me one day before this beerup!S</span>
+              </div>
+            </div>
+            <button className="submit" type="submit">Join up</button>
           </form>
         </div>
       </section>

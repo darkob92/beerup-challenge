7010f46de6111e6be28664f0b39d9ef011068cfb
Darko <darko.bogovic92@gmail.com>
2019-05-12 18:10:40 +0200

    added media queries for lower resolution screens and menu

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -12,7 +12,7 @@ const Banner = () => {
         <div className="banner__title-box">
           <h5>The Beersteer pro 2.0</h5>
           <h1 className="title title--primary">Join our famous beerup!</h1>
-          <Link className="btn btn--small" to="/signup">Join beerup</Link>
+          <Link className="btn btn--small btn__banner" to="/signup">Join beerup</Link>
         </div>
         <div className="banner__img">
           <img alt="beers" src={Beers} />
--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,24 +1,60 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './navigation.scss';
 import Logo from '../../assets/logo.svg';
 import Symbol from '../../assets/symbol.svg';
 import { NavLink } from 'react-router-dom';
 
-const Navigation = () => {
-  return(
-    <nav className="navigation">
-      <div className="navigation__logo-box">
-        <img alt="symbol" className="logo" src={Symbol} />
-        <img alt="logo" className="logo" src={Logo} />
-      </div>
+class Navigation extends Component {
+  state = {
+    toggle: false
+  }
 
-      <ul className="navigation__list">
-        <li className="navigation__item"><NavLink className="navigation__link" to="/">Home</NavLink></li>
-        <li className="navigation__item"><NavLink className="navigation__link" to="/favorites">Favorites</NavLink></li>
-        <li className="navigation__item"><NavLink className="navigation__link" to="/signup">Join</NavLink></li>
-      </ul>
-    </nav>
-  );
+  toggleMenu = () => {
+    const currentState = this.state.toggle;
+    this.setState({
+      toggle: !currentState
+    })
+  } 
+
+
+  render() {
+    return (
+      <nav className="navigation">
+        <div className="navigation__logo-box">
+          <img alt="symbol" className="logo" src={Symbol} />
+          <img alt="logo" className="logo" src={Logo} />
+        </div>
+
+        <ul className="navigation__list">
+          <li className="navigation__item"><NavLink className="navigation__link" to="/">Home</NavLink></li>
+          <li className="navigation__item"><NavLink className="navigation__link" to="/favorites">Favorites</NavLink></li>
+          <li className="navigation__item"><NavLink className="navigation__link" to="/signup">Join</NavLink></li>
+        </ul>
+
+        {/* hamburger menu for mobile devices */}
+
+          {this.state.toggle ? 
+          <div className="navigation__menu change" onClick={this.toggleMenu}>
+            <div className="bar1"></div>
+            <div className="bar2"></div>
+            <div className="bar3"></div>
+          </div>
+           : 
+          <div className="navigation__menu" onClick={this.toggleMenu}>
+            <div className="bar1"></div>
+            <div className="bar2"></div>
+            <div className="bar3"></div>
+          </div>
+
+        }
+        {this.state.toggle ? 
+          <ul className="menu-list">
+            <li>Home</li>
+            <li>Favorites</li>
+        </ul> : null} 
+      </nav>
+    );
+  }
 }
 
 export default Navigation;

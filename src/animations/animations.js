fa7b3d85700d9055a2237ae936b70d70ffe39fd6
Darko <darko.bogovic92@gmail.com>
2019-05-13 17:10:24 +0200

    added title animation

--- src/animations/animations.js
+++ src/animations/animations.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import { Tween, Timeline } from 'react-gsap';
-import { Power1 } from 'gsap';
+import { Power1, Power2 } from 'gsap';
 
 const BeerAnimation = (props) => {
   return(
-    <Tween from={{y: '-500', opacity: 0, duration: '1', ease: Power1.easeOut}}>
+    <Tween from={{y: '-500', opacity: 0, duration: 1, ease: Power1.easeOut}}>
+      {props.children}
+    </Tween>
+  );
+}
+
+const TitleAnimation = (props) => {
+  return(
+    <Tween from={{ opacity: 0, delay: 1.5, duration: 1.5, ease: Power1.easeOut}}>
       {props.children}
     </Tween>
   );
@@ -12,7 +20,7 @@ const BeerAnimation = (props) => {
 
 const NavAnimation = (props) => {
   return(
-    <Tween from={{y: '100', opacity: 0, delay: 1, ease: Power1.easeOut}}>
+    <Tween from={{y: '100', opacity: 0, delay: 1, ease: Power2.easeOut}}>
       {props.children}
     </Tween>
   );
@@ -20,5 +28,6 @@ const NavAnimation = (props) => {
 
 export {
   BeerAnimation,
-  NavAnimation
+  NavAnimation,
+  TitleAnimation
 };
--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -4,17 +4,19 @@ import './banner.scss';
 import Beers from '../../assets/illustration.svg';
 import Cap from '../../assets/cap.svg';
 import { Link } from 'react-router-dom';
-import { BeerAnimation } from '../../animations/animations';
+import { BeerAnimation, TitleAnimation } from '../../animations/animations';
 
 const Banner = () => {
   return(
     <section className="banner-section">
       <div className="banner">
-        <div className="banner__title-box">
-          <h5>The Beersteer pro 2.0</h5>
-          <h1 className="title title--primary">Join our famous beerup!</h1>
-          <Link className="btn btn--small btn__banner" to="/signup">Join beerup</Link>
-        </div>
+        <TitleAnimation>
+          <div className="banner__title-box">
+            <h5>The Beersteer pro 2.0</h5>
+            <h1 className="title title--primary">Join our famous beerup!</h1>
+            <Link className="btn btn--small btn__banner" to="/signup">Join beerup</Link>
+          </div>
+        </TitleAnimation>
         <div className="banner__img">
           <BeerAnimation>
             <img alt="beers" src={Beers} />
